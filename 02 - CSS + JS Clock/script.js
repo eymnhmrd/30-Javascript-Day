9096623af72a1c3fa55131f65760d964933b91b2
eymnhmrd <aymenhamrad@gmail.com>
2022-09-29 02:20:41 +0100

    Completed Clock challenge

--- 02 - CSS + JS Clock/script.js
+++ 02 - CSS + JS Clock/script.js
@@ -1,6 +1,23 @@
 
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
 
 const SetDate = () => {
     const now = new Date();
-    const seconds = now.getSeconds;
+
+    const seconds = now.getSeconds();
+    const secondsDeg = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDeg = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDeg}deg)`;
+
+    const hours = now.getHours();
+    const hoursDeg = ((hours / 12) * 360) + 90;
+    hoursHand.style.transform = `rotate(${hoursDeg}deg)`;
+
 }
+
+setInterval(SetDate,1000);
